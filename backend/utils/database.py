9b0ed21110be4a5f01450259adeaf90e256f553b
janupatiljanupatil127-create <janupatiljanupatil127@gmail.com>
2026-07-14 23:11:36 +0530

    Implement database creation and prediction saving
    Added functions to create a database and save predictions.

--- backend/utils/database.py
+++ backend/utils/database.py
@@ -1,1 +1,90 @@
 
+import sqlite3
+
+DATABASE = "history.db"
+
+
+def create_database():
+
+    connection = sqlite3.connect(DATABASE)
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS predictions(
+
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+            image_name TEXT,
+
+            random_forest TEXT,
+
+            svm TEXT,
+
+            knn TEXT,
+
+            voting TEXT,
+
+            prediction_time REAL,
+
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+        )
+    """)
+
+    connection.commit()
+
+    connection.close()
+
+
+def save_prediction(
+    image_name,
+    rf,
+    svm,
+    knn,
+    voting,
+    prediction_time
+):
+
+    connection = sqlite3.connect(DATABASE)
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        INSERT INTO predictions(
+
+            image_name,
+
+            random_forest,
+
+            svm,
+
+            knn,
+
+            voting,
+
+            prediction_time
+
+        )
+
+        VALUES(?,?,?,?,?,?)
+
+    """, (
+
+        image_name,
+
+        rf,
+
+        svm,
+
+        knn,
+
+        voting,
+
+        prediction_time
+
+    ))
+
+    connection.commit()
+
+    connection.close()
